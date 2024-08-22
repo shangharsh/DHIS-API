ff975b7c3e26fc70b4bacf2092078cc4b044bcd4
Shangharsh Adhikari <san.adh1996@gmail.com>
2024-08-22 12:31:54 +0545

    Feat: 10-01 Outreach Clinic and Community Health (NEW)

---
+++ 10-01 Outreach Clinic and Community Health (NEW)/OutreachClinicandCommunityHealth(NEW).Designer.vb
@@ -0,0 +1,343 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class OutreachClinicandCommunityHealth_NEW_
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(OutreachClinicandCommunityHealth_NEW_))
+        Me.TextBox25 = New System.Windows.Forms.TextBox()
+        Me.TextBox24 = New System.Windows.Forms.TextBox()
+        Me.TextBox23 = New System.Windows.Forms.TextBox()
+        Me.TextBox22 = New System.Windows.Forms.TextBox()
+        Me.TextBox21 = New System.Windows.Forms.TextBox()
+        Me.TextBox20 = New System.Windows.Forms.TextBox()
+        Me.TextBox19 = New System.Windows.Forms.TextBox()
+        Me.TextBox18 = New System.Windows.Forms.TextBox()
+        Me.TextBox17 = New System.Windows.Forms.TextBox()
+        Me.TextBox16 = New System.Windows.Forms.TextBox()
+        Me.TextBox15 = New System.Windows.Forms.TextBox()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox25
+        '
+        Me.TextBox25.Location = New System.Drawing.Point(456, 512)
+        Me.TextBox25.Name = "TextBox25"
+        Me.TextBox25.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox25.TabIndex = 2
+        '
+        'TextBox24
+        '
+        Me.TextBox24.Location = New System.Drawing.Point(456, 461)
+        Me.TextBox24.Name = "TextBox24"
+        Me.TextBox24.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox24.TabIndex = 2
+        '
+        'TextBox23
+        '
+        Me.TextBox23.Location = New System.Drawing.Point(456, 401)
+        Me.TextBox23.Name = "TextBox23"
+        Me.TextBox23.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox23.TabIndex = 2
+        '
+        'TextBox22
+        '
+        Me.TextBox22.Location = New System.Drawing.Point(456, 344)
+        Me.TextBox22.Name = "TextBox22"
+        Me.TextBox22.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox22.TabIndex = 2
+        '
+        'TextBox21
+        '
+        Me.TextBox21.Location = New System.Drawing.Point(456, 301)
+        Me.TextBox21.Name = "TextBox21"
+        Me.TextBox21.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox21.TabIndex = 2
+        '
+        'TextBox20
+        '
+        Me.TextBox20.Location = New System.Drawing.Point(456, 266)
+        Me.TextBox20.Name = "TextBox20"
+        Me.TextBox20.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox20.TabIndex = 2
+        '
+        'TextBox19
+        '
+        Me.TextBox19.Location = New System.Drawing.Point(456, 233)
+        Me.TextBox19.Name = "TextBox19"
+        Me.TextBox19.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox19.TabIndex = 2
+        '
+        'TextBox18
+        '
+        Me.TextBox18.Location = New System.Drawing.Point(456, 203)
+        Me.TextBox18.Name = "TextBox18"
+        Me.TextBox18.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox18.TabIndex = 2
+        '
+        'TextBox17
+        '
+        Me.TextBox17.Location = New System.Drawing.Point(456, 174)
+        Me.TextBox17.Name = "TextBox17"
+        Me.TextBox17.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox17.TabIndex = 2
+        '
+        'TextBox16
+        '
+        Me.TextBox16.Location = New System.Drawing.Point(456, 142)
+        Me.TextBox16.Name = "TextBox16"
+        Me.TextBox16.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox16.TabIndex = 2
+        '
+        'TextBox15
+        '
+        Me.TextBox15.Location = New System.Drawing.Point(456, 106)
+        Me.TextBox15.Name = "TextBox15"
+        Me.TextBox15.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox15.TabIndex = 2
+        '
+        'TextBox14
+        '
+        Me.TextBox14.Location = New System.Drawing.Point(456, 73)
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox14.TabIndex = 2
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Location = New System.Drawing.Point(456, 44)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox13.TabIndex = 2
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(206, 498)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox12.TabIndex = 2
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(206, 431)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox11.TabIndex = 2
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(207, 363)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox10.TabIndex = 2
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(206, 314)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox9.TabIndex = 2
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(207, 273)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox8.TabIndex = 2
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(206, 234)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox7.TabIndex = 2
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox6.Location = New System.Drawing.Point(206, 209)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(39, 18)
+        Me.TextBox6.TabIndex = 2
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(207, 185)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox5.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(206, 155)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox4.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(206, 124)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(206, 93)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(40, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(206, 55)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(64, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(47, 30)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Push"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(48, 31)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Load"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(513, 540)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox25)
+        Me.GroupBox1.Controls.Add(Me.TextBox24)
+        Me.GroupBox1.Controls.Add(Me.TextBox23)
+        Me.GroupBox1.Controls.Add(Me.TextBox22)
+        Me.GroupBox1.Controls.Add(Me.TextBox21)
+        Me.GroupBox1.Controls.Add(Me.TextBox20)
+        Me.GroupBox1.Controls.Add(Me.TextBox19)
+        Me.GroupBox1.Controls.Add(Me.TextBox18)
+        Me.GroupBox1.Controls.Add(Me.TextBox17)
+        Me.GroupBox1.Controls.Add(Me.TextBox16)
+        Me.GroupBox1.Controls.Add(Me.TextBox15)
+        Me.GroupBox1.Controls.Add(Me.TextBox14)
+        Me.GroupBox1.Controls.Add(Me.TextBox13)
+        Me.GroupBox1.Controls.Add(Me.TextBox12)
+        Me.GroupBox1.Controls.Add(Me.TextBox11)
+        Me.GroupBox1.Controls.Add(Me.TextBox10)
+        Me.GroupBox1.Controls.Add(Me.TextBox9)
+        Me.GroupBox1.Controls.Add(Me.TextBox8)
+        Me.GroupBox1.Controls.Add(Me.TextBox7)
+        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(33, 24)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(519, 554)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'OutreachClinicandCommunityHealth_NEW_
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(593, 605)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "OutreachClinicandCommunityHealth_NEW_"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "OutreachClinicandCommunityHealth_NEW_"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TextBox25 As TextBox
+    Friend WithEvents TextBox24 As TextBox
+    Friend WithEvents TextBox23 As TextBox
+    Friend WithEvents TextBox22 As TextBox
+    Friend WithEvents TextBox21 As TextBox
+    Friend WithEvents TextBox20 As TextBox
+    Friend WithEvents TextBox19 As TextBox
+    Friend WithEvents TextBox18 As TextBox
+    Friend WithEvents TextBox17 As TextBox
+    Friend WithEvents TextBox16 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ 10-01 Outreach Clinic and Community Health (NEW)/OutreachClinicandCommunityHealth(NEW).vb
@@ -0,0 +1,221 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Newtonsoft.Json.Linq
+
+Public Class OutreachClinicandCommunityHealth_NEW_
+    Dim dataSetId As String = "efBGIBcySCl" 'UID of Dataset
+    Dim todayDate As String = Date.Now().ToString("yyyy-MM-dd") 'Today Date
+    Dim period As String = "208102" 'Eg: 208101 for 2080 Baisakh
+    Dim orgUnitId As String = "aUv4lHwAFh9" 'Eg: Hospital UID
+    Dim attributeOptionCombo As String = "" 'Only for ICD11 disease, UID of ICD11 disease
+
+    Dim apiUrl As String = "https://hmis.gov.np/hmisdemo/api/dataValueSets"
+    Dim userName As String = "baghauda.hosp"
+    Dim passWord As String = "Hmis@3344"
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim jsonData As String = "{
+    ""dataSet"": """ & dataSetId & """, 
+    ""completeDate"": """ & todayDate & """, 
+    ""period"": """ & period & """, 
+    ""orgUnit"": """ & orgUnitId & """, 
+    ""attributeOptionCombo"": """", 
+    ""dataValues"": [
+{" &
+        """dataElement"": ""G3LhqoHfxhv""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox1.Text & """," &
+        """comment"": ""प्राथमिक उपचार गरेका """ &
+    "}," &
+    "{" &
+        """dataElement"": ""Eyf5Gkn3Y9j""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox2.Text & """," &
+        """comment"": ""०-११ महिना -सामान्य """ &
+    "}," &
+    "{" &
+        """dataElement"": ""O3FP5FMZiHk""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox3.Text & """," &
+        """comment"": ""०-११ महिना -जोखिम """ &
+    "}," &
+    "{" &
+        """dataElement"": ""LDGiXGfVRO1""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox4.Text & """," &
+        """comment"": ""०-११ महिना -अति जोखिम  """ &
+    "}," &
+    "{" &
+        """dataElement"": ""F381UkDst7i""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox5.Text & """," &
+        """comment"": ""१२-२३ महिना -सामान्य """ &
+    "}," &
+    "{" &
+        """dataElement"": ""N2aPjNVGRxK""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox6.Text & """," &
+        """comment"": ""१२-२३ महिना -जोखिम """ &
+    "}," &
+    "{" &
+        """dataElement"": ""QfZokURtxfz""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox7.Text & """," &
+        """comment"": ""१२-२३ महिना -अति जोखिम """ &
+    "}," &
+    "{" &
+        """dataElement"": ""AmFg04KWFno""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox8.Text & """," &
+        """comment"": ""गर्भ जाँच गरेका महिला """ &
+    "}," &
+    "{" &
+        """dataElement"": ""B6OgKkUhpe5""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox9.Text & """," &
+        """comment"": ""सुत्केरी जाँच गरेका महिला """ &
+    "}," &
+    "{" &
+        """dataElement"": ""PKj90nRaglb""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox10.Text & """," &
+        """comment"": ""जुकाको औषधी पाएका गर्भवति """ &
+    "}," &
+    "{" &
+        """dataElement"": ""S1YXstoZbGN""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox11.Text & """," &
+        """comment"": ""जन्मेको ६ महिनासम्म स्तनपान मात्र गराएको """ &
+    "}," &
+    "{" &
+        """dataElement"": ""hkAUC5M7nIQ""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox12.Text & """," &
+        """comment"": ""६ महिनापछि स्तनपानका साथै ठोस, अर्धठोस र नरम खाना सुरु गरेका """ &
+    "}," &
+    "{" &
+        """dataElement"": ""uoCe6cHZwnx""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox13.Text & """," &
+        """comment"": ""आइरन चक्की वितरण गराएको --नयाँ गर्भवती """ &
+    "}," &
+    "{" &
+        """dataElement"": ""BOdlGFIuzJz""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox14.Text & """," &
+        """comment"": ""आइरन चक्की वितरण गराएको --दोहोर्याइ आएका """ &
+    "}," &
+    "{" &
+        """dataElement"": ""FxKnFTm6fwc""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox15.Text & """," &
+        """comment"": ""आइरन चक्की वितरण गराएको --सुत्केरी महिला  """ &
+    "}," &
+    "{" &
+        """dataElement"": ""mm7gqOgzo8b""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox16.Text & """," &
+        """comment"": ""भिटामिन ए पाएका सुत्केरी महिला""" &
+    "}," &
+    "{" &
+        """dataElement"": ""cRDagjiFlVQ""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox17.Text & """," &
+        """comment"": ""कण्डम गोटा""" &
+    "}," &
+    "{" &
+        """dataElement"": ""uWUt9X4pLeE""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox18.Text & """," &
+        """comment"": ""पिल्स जना""" &
+    "}," &
+    "{" &
+        """dataElement"": ""LkrqEnZWEpb""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox19.Text & """," &
+        """comment"": ""पिल्स साईकल""" &
+    "}," &
+    "{" &
+        """dataElement"": ""SSbMoJcQAid""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox20.Text & """," &
+        """comment"": ""डिपो  डोज""" &
+    "}," &
+    "{" &
+        """dataElement"": ""QAGDdsrckuH""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox21.Text & """," &
+        """comment"": ""सायना प्रेस डोज""" &
+    "}," &
+    "{" &
+        """dataElement"": ""EyqFw1wZqva""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox22.Text & """," &
+        """comment"": ""आकस्मिक चक्की डोज """ &
+    "}," &
+    "{" &
+        """dataElement"": ""LRbClvpaMOD""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox23.Text & """," &
+        """comment"": ""उपचारमा नियमित नभएका बिरामीको खोज गरेको संख्या (क्षयरोग)""" &
+    "}," &
+    "{" &
+        """dataElement"": ""A481oDgQj8q""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox24.Text & """," &
+        """comment"": ""रक्त नमुना संकलन गरेको स्लाइड संख्या""" &
+    "}," &
+    "{" &
+        """dataElement"": ""lp0OcKxZDMW""," &
+        """categoryOptionCombo"": ""kdsirVNKdhm""," &
+        """value"": """ & TextBox25.Text & """," &
+        """comment"": ""आमा समुहको वैठकमा भाग लिएको """ &
+    "}" &
+"]" &
+    "}"
+
+        Dim response As String = SubmitData(apiUrl, userName, passWord, jsonData)
+
+    End Sub
+
+    Public Shared Function SubmitData(ByVal apiUrl, ByVal userName, ByVal passWord, ByVal jsonData)
+        Dim ReturnValue As String = ""
+        Dim request As HttpWebRequest = CType(WebRequest.Create(apiUrl), HttpWebRequest)
+        request.Method = "POST"
+        request.ContentType = "application/json"
+        Dim credentials As String = Convert.ToBase64String(Encoding.ASCII.GetBytes(userName & ":" + passWord))
+        request.Headers(HttpRequestHeader.Authorization) = "Basic " & credentials
+
+        Try
+
+            Using writer As StreamWriter = New StreamWriter(request.GetRequestStream())
+                writer.Write(jsonData)
+                writer.Flush()
+            End Using
+
+            Using response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
+
+                Using responseStream As Stream = response.GetResponseStream()
+
+                    Using reader As StreamReader = New StreamReader(responseStream)
+                        Dim responseJson As String = reader.ReadToEnd()
+                        Dim responseObject As Object = JObject.Parse(responseJson)
+                        Dim status As String = responseObject("description").ToString()
+                        MsgBox(status)
+
+                    End Using
+                End Using
+            End Using
+
+        Catch ex As WebException
+            ReturnValue = ex.Message
+            MsgBox(ReturnValue)
+
+        End Try
+
+        Return ReturnValue
+    End Function
+
+End Class
